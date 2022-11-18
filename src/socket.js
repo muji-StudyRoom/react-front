658d8064de5b4e72625e495f065ef62564541f76
heejae <rhj0830@gmail.com>
2022-11-18 15:14:44 +0900

    오타수정

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
-const url = proces.env.REACT_APP_BACK_URL;
+const url = process.env.REACT_APP_BACK_URL;
 export const socket = io(url, {
     cors: {
         origin: "*",
