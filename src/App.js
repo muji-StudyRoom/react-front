27da00aa97356a434e97eac38d67a340732972cb
energy-been <lkb980316@gmail.com>
2022-11-22 12:26:15 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './page/MainPage';
 import MeetingPage from './page/MeetingPage'
 
-// export const SetRoomContext = createContext(() => {})
 
-// !! route 경로에 /meeting 을 제거해야 함 -> 직접적인 접근 제한 ?? 해당 코드 삭제 시 modal 창에서 navigate로 meeting으로 이동할 수 있을 지 확인해보아야 함
 function App() {
   return (
     <BrowserRouter>
