99378925fcd4f4f5296ff2b2ed271d2fdaf558f5
energy-been <lkb980316@gmail.com>
2022-11-25 16:49:54 +0900

    setupProxy modify

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware')
 module.exports = function(app) {
     app.use(
         proxy('/room', {
-            target: "back-svc", // 비즈니스 서버 URL 설정
+            target: "http://back-svc:8080", // 비즈니스 서버 URL 설정
             changeOrigin: true
         })
     );
