004305a44f300b95588968479a41e25b389e03c9
heejae <rhj0830@gmail.com>
2022-11-27 21:48:53 +0900

    testing react

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io("k8s-default-sigsvc-b3e52a2381-0d714db26662c7b9.elb.ap-northeast-2.amazonaws.com:5000", {
+export const socket = io("https://rohee.tk:5000", {
     cors: {
         origin: "*",
     },
