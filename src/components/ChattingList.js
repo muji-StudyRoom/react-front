0991aa5f6f69ddacdd438a67b0e0b3c970263e65
energy-been <lkb980316@gmail.com>
2022-12-01 15:28:33 +0900

    dm modify

--- src/components/ChattingList.js
+++ src/components/ChattingList.js
@@ -8,43 +8,46 @@ const ChattingList = React.memo(() => {
         let name_element = document.createElement("li");
         let chat_element = document.createElement("li");
         let space_element = document.createElement("li");
+        let chat_scroll = document.getElementById("chat_scroll");
         space_element.innerText = "\n"
+        
         console.log(typeof (data))
         if (data["type"] === "disconnect") {
             name_element.className = "name_element";
             chat_element.className = "notice_element"
             name_element.innerText = "알림"
-            chat_element.innerText = data["text"];
-            document.getElementById("chat_scroll").appendChild(name_element)
-            document.getElementById("chat_scroll").appendChild(chat_element)
-            document.getElementById("chat_scroll").appendChild(space_element)
+            chat_element.innerText = data["name"] + " 님이 퇴장하셨습니다.";
+            chat_scroll.appendChild(name_element)
+            chat_scroll.appendChild(chat_element)
+            chat_scroll.appendChild(space_element)
+            document.getElementById(data["sid"]).remove();
         }
         else if (data["type"] === "join") {
             name_element.className = "name_element";
-            chat_element.className = "notice_element"
             name_element.innerText = "알림"
-            chat_element.innerText = data["text"];
-            document.getElementById("chat_scroll").appendChild(name_element)
-            document.getElementById("chat_scroll").appendChild(chat_element)
-            document.getElementById("chat_scroll").appendChild(space_element)
+            chat_element.className = "notice_element"
+            chat_element.innerText = data["name"] + " 님이 입장하셨습니다.";
+            chat_scroll.appendChild(name_element)
+            chat_scroll.appendChild(chat_element)
+            chat_scroll.appendChild(space_element)
             
             let dm_select = document.createElement("option");
-            dm_select.value = data["sender"];
-            dm_select.innerText = data["sender"]
+            dm_select.id = data["sid"]
+            dm_select.value = data["sid"];
+            dm_select.innerText = data["name"]
             console.log(document.getElementById("dm-select"))
             console.log(dm_select)
-            // document.getElementById("dm-select").appendChild(dm_select);
+            document.getElementById("dm-select").appendChild(dm_select);
         }
         else {
             name_element.className = "name_element";
             chat_element.className = "chat_element";
             name_element.innerText = data["sender"]
             chat_element.innerText = data["text"];
-            document.getElementById("chat_scroll").appendChild(name_element)
-            document.getElementById("chat_scroll").appendChild(chat_element)
-            document.getElementById("chat_scroll").appendChild(space_element)
+            chat_scroll.appendChild(name_element)
+            chat_scroll.appendChild(chat_element)
+            chat_scroll.appendChild(space_element)
         }
-
         document.getElementById("chat_scroll").scroll({ top: document.getElementById("chat_scroll").scrollHeight, behavior: 'smooth' })
     })
 
